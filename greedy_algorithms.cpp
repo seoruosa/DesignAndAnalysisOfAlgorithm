061fc5bdd912a1531ebb50e3e408fab2aafa5e27
Thiago <thiago.giachetto@gmail.com>
2021-11-10 21:56:20 -0300

    create a new example of graph to calculate the MSP and add kruskal algorithm

--- greedy_algorithms.cpp
+++ greedy_algorithms.cpp
@@ -2,20 +2,44 @@
 #include <vector>
 #include "util.cpp"
 #include "prim.cpp"
+#include "kruskal.cpp"
 
 int main()
-{
-
+{   
+    // std::vector<std::vector<float>> graph({
+    //     {INF, 4, 9, 5},
+    //     {4, INF, 7, 6},
+    //     {9, 7, INF, 8},
+    //     {5, 6, 8, INF},
+    // });
+    
+    
     std::vector<std::vector<float>> graph({
-        {INF, 4, 9, 5},
-        {4, INF, 7, 6},
-        {9, 7, INF, 8},
-        {5, 6, 8, INF},
+      // a, b, c, d, e, f
+        {INF, 3  , INF  , INF, 6    , 5},
+        {3  , INF, 1    , INF, INF  , 4},
+        {INF, 1  , INF  , 6  , INF  , 4},
+        {INF, INF, 6    , INF, 8    , 5},
+        {6  , INF, INF  , 8  , INF  , 2},
+        {5  , 4  , 4    , 5  , 2    , INF},
     });
+
+    std::cout << "Problem: " << std::endl;
+    print_matrix(graph);
 
     auto result = prim_n2(graph);
 
+    std::cout << "prim result: " << std::endl;
     print_matrix(result);
+
+    auto result_kruskal = kruskal(graph);
+
+    std::cout << "kruskal result: " << std::endl;
+
+    for (auto edge : result_kruskal)
+    {
+        std::cout << edge << std::endl;
+    }
 
     // auto a = Node({0, 1.1});
     // auto b = Node({1, 1.5});
